aec878eedb1cc21d8771bdcdb34a314929ba6469
ntagungira_ali <ntagungiraali@gmail.com>
2021-06-16 09:41:49 +0200

    Token generation

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
+int generate_token();
 int main()
 {
-
-
-
+ generate_token();  
+}
+int generate_token(){
+   srand(time(0));
+     printf("Token is %d-%d-%d",rand());
     return 0;
 }
